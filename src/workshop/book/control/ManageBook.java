903fcee7ab4672d8ff51c2a2b06491a4200114d3
intaekim114 <121932300+intaekim114@users.noreply.github.com>
2025-04-24 09:51:59 +0900

    4월 24일

---
+++ src/workshop/book/control/ManageBook.java
@@ -0,0 +1,30 @@
+package workshop.book.control;
+
+import workshop.book.entity.Magazine;
+import workshop.book.entity.Novel;
+import workshop.book.entity.Publication;
+import workshop.book.entity.ReferenceBook;
+
+public class ManageBook {
+
+	public static void main(String[] args) {
+		//Publication 타입 배열을 선언 및 생성
+		Publication[] pubs = new Publication[5];
+		pubs[0] = new Magazine("마이크로소프트","2007-10-01",328,9900,"매월");
+		pubs[0] = new Magazine("경영과컴퓨터","2007-10-03",316,9000,"매월");
+		pubs[0] = new Novel("빠삐용","2007-07-01",396,9800,"베르나르베르베르","현대소설");
+		pubs[0] = new Novel("남한산성","2007-04-14",383,11000,"김훈","대하소설");
+		pubs[0] = new ReferenceBook("실용주의프로그래머","2007-01-14",496,25000,"소프트웨어공학");
+		 
+		//Magazine 객체생성
+		Magazine mz = new Magazine();
+		//mz 변수로 호출 가능한 메서드는 5개
+		Publication pub = new Magazine();
+		//pub 변수로 호출 가능한 메서드 4개
+		
+		//Novel 객체생성
+		Novel novel = new Novel();
+		Publication pub2 = new Novel();
+	}
+
+}
